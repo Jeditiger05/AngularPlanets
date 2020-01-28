c97cd9ea710a3634d464ba5a2874805cd0c5c6ea
Jeditiger05 <john@jedimedia.it>
2020-01-29 10:33:07 +1100

    Added 3 Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlanetsListComponent } from './planets-list/planets-list.component';
+import { PlanetsCreateComponent } from './planets-create/planets-create.component';
+import { PlanetsDetailComponent } from './planets-detail/planets-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsListComponent,
+    PlanetsCreateComponent,
+    PlanetsDetailComponent
   ],
   imports: [
     BrowserModule
