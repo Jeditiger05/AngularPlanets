06c73f672f64d95c87d2cb7ceb3660970563893e
Jeditiger05 <john@jedimedia.it>
2020-01-30 17:38:22 +1100

    Fisnished Tutorial

--- src/app/planets-create/planets-create.component.ts
+++ src/app/planets-create/planets-create.component.ts
@@ -9,18 +9,22 @@ import { Planet } from '../planet';
 })
 export class PlanetsCreateComponent implements OnInit {
 
-  name: string;
+  planet1: Planet;
+/*   name: string;
   moons: number;
-  distanceFromSun: number;
+  distanceFromSun: number; */
 
   constructor(private store: PlanetStoreService) { }
 
   ngOnInit() {
   }
 
-  createPlanet(){
-    let planet1 = new Planet(this.name, this.moons, this.distanceFromSun);
-    this.store.addPlanet(planet1);
+  createPlanet() {
+   // let planet1 = new Planet(this.name, this.moons, this.distanceFromSun);//Create Planet Object Using constructor
+   // let planet2: Planet = { name: this.name, moons: this.moons, distanceFromSun: this.distanceFromSun }//Create Planet Object without Using Constructor
+
+    this.store.addPlanet(this.planet1);
+    //this.store.addPlanet(planet2);
     this.store.log();
 
   }
